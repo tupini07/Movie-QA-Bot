2a138eace012be6bd0158373f105bb897834f9b8
Andrea Tupini <tupini07@gmail.com>
2018-06-23 11:08:46 +0200

    a bit of clarification on how voice behaves
    Speaking for too long can be tiersom for the user, so the bot only reads
    the first part of an answer and posts the rest to the console

--- channels.py
+++ channels.py
@@ -18,14 +18,14 @@ class VoiceOutputChannel(OutputChannel):
     def send_text_message(self, recipient_id, message):
         # type: (Text, Text) -> None
         what_to_say = message
-        if len(what_to_say) > 250: # if we're going to say something very long then truncate it
-            what_to_say = "I found to much results to speak them, so I'll just print them to the console instead"
+        if len(what_to_say) > 250:  # if we're going to say something very long then truncate it
+            what_to_say = "I found to much information to speak them, so I'll just print them to the console instead"
 
         what_to_say = what_to_say.split("\n")[0]
 
         self.TTSENGINE.say(what_to_say)
         print("Bot: " + message)
-        self.TTSENGINE.runAndWait() 
+        self.TTSENGINE.runAndWait()
 
 
 class VoiceInputChannel(InputChannel):
@@ -40,28 +40,36 @@ class VoiceInputChannel(InputChannel):
             self.SRECOGNIZER = sr.Recognizer()
             self.MICROPHONE = sr.Microphone()
 
-
     def _record_messages(self, on_message, max_message_limit=None):
-        utils.print_color("Bot loaded with voice capabilities!",
+        utils.print_color("Bot loaded with voice capabilities! \n" +
+                          "Note that in this mode the bot will do the following things: \n" +
+                          "- When the answer it's too long (more than 250 characters) the bot will " +
+                          "just say that it found an answer and then print the real answer to the console "+
+                          "this is done because 'speaking' a long answer can take a lot of time \n" +
+                          "- When multiple answers to a question are found then the bot will only say that it found multiple " + 
+                          "answers and it will print each answer to the console. This is also done to save time (besides, " +
+                          "listening to multiple answers can be confusing)" ,
                           utils.bcolors.OKGREEN)
         num_messages = 0
         while max_message_limit is None or num_messages < max_message_limit:
             try:
-                if self.prefer_text_input: ## if we want to do normal text input and have audio output:
+                if self.prefer_text_input:  # if we want to do normal text input and have audio output:
                     text = input("\nYour Input: ").strip()
-                    
-                else: # if we want to use text input as well
+
+                else:  # if we want to use text input as well
                     with self.MICROPHONE as source:
                         self.SRECOGNIZER.adjust_for_ambient_noise(source)
-                        input("\nBot is ready! Press enter when you're ready to speak.")
+                        input(
+                            "\nBot is ready! Press enter when you're ready to speak.")
                         audio = self.SRECOGNIZER.listen(source)
-                    
+
                     print("Bot is recognizing what you said ...")
                     text = self.SRECOGNIZER.recognize_google(audio)
                     print("You said: " + text)
 
             except Exception:
-                text = input("Voice does not work :( input your query here: ").strip()
+                text = input(
+                    "Voice does not work :( input your query here: ").strip()
 
             if six.PY2:
                 # in python 2 input doesn't return unicode values
